8080cfef1065fb660b7cc193e9f32a36d3516bd7
Matthew Caseres <matthewcaseres@outlook.com>
2019-05-27 06:37:54 -0700

    Create Project
    I need to switch computers because windows keeps being weird. Pushing to github.

---
+++ R/LongMortalityTables-package.R
@@ -0,0 +1,9 @@
+#' LongMortalityTables: package for long mortality tables
+#'
+#' Creation of an exposure table with rows for policy-intervals from a table
+#' with a unique policy number key and beginning and ending dates for each policy.
+#' Methods for assigning supplemental data containing dates and policy numbers
+#' to the corresponding interval from the created exposures table.
+#'
+#' @docType package
+#' @name LongMortalityTables
